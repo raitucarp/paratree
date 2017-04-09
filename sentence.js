c7c9c18e37add135fa6c1fc11f5b9da310033fe5
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2017-04-10 00:38:11 +0700

    Add more methods and Refactor code
    Constructor now accept first argument as paragraph parent. Also create a reference from its parent.
    
    parseWords now pass a sentence rather than sentenceId.
    
    parent() method get paragraph parent from a sentence.
    
    This commit also introduce lookup methods, they are:
    - findWordById = find a word by its id
    - findWordByIndex = find a word by its index
    
    Sentence has navigate method too:
    - next() is for navigating to the next sentence in a paragraph context.
    - prev() is for navigating to the previous sentence in a paragraph context.

--- sentence.js
+++ sentence.js
@@ -4,14 +4,40 @@ const Word = require('./word');
 
 
 let words = new WeakMap();
+let parent = new WeakMap();
+
 class Sentence {
-  constructor(paragraphId, text, index) {
+  constructor(paragraph, text, index) {
+    parent.set(this, paragraph);
+
     this.id = uuidV4();
     this.index = index;
-    this.paragraphId = paragraphId;
+    this.paragraphId = paragraph.id;
     this.text = text;
     this.parseWords();
     return this;
+  }
+
+  parent() {
+    return parent.get(this);
+  }
+
+  findWordById(id) {
+    return _.find(this.wordsCollection, {id});
+  }
+
+  findWordByIndex(index) {
+    return _.find(this.wordsCollection, {index});
+  }
+
+  next() {
+    let index = this.index;
+    return this.parent().findSentenceByIndex(index + 1);
+  }
+
+  prev() {
+    let index = this.index;
+    return this.parent().findSentenceByIndex(index - 1);
   }
 
   get words() {
@@ -22,14 +48,15 @@ class Sentence {
     words.set(this, w);
   }
 
-  get wordsObject() {
+  get wordsCollection() {
     return words.get(this);
   }
 
   parseWords() {
-    let {id: sentenceId, text} = this;
+    let sentence = this;
+    let {text} = sentence;
     this.words = _.chain(text).split(/\s{1,}/ig)
-      .map((text, index) => new Word(sentenceId, index, text)).value();
+      .map((text, index) => new Word(sentence, index, text)).value();
   }
 }
 
