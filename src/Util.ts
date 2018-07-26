b96db3af8ccd1594a6964f10fddf3cd184c88a52
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2018-07-26 13:48:30 +0700

    Add/change some definition and  types in main source file

--- src/Util.ts
+++ src/Util.ts
@@ -1,48 +1,54 @@
 import Sentence from "./Sentence";
 import Word from "./Word";
-import _ from 'lodash';
+import _ from "lodash";
 import { IParaNode, IParaNodeMethod } from "./interfaces";
 
-
-export function findChildNodeByProps<T extends IParaNode>(child: T[], props: keyof IParaNode, value: T[keyof IParaNode]): T | null {
-    return _.find(child, (o: T) => o[props] == value) || null;
+export function findChildNodeByProps<T extends IParaNode>(
+  child: T[],
+  props: keyof IParaNode,
+  value: T[keyof IParaNode]
+): T | null {
+  return _.find(child, (o: T) => o[props] == value) || null;
 }
 
-
-export function nextNode<P extends IParaNode & IParaNodeMethod<P, T>, T extends IParaNode>(
-    parent: P, onlyInParent?: boolean
-): T | null {
-    const { index } = parent;
-    const nextNode = parent.findChildrenByIndex(index + 1);
+export function nextNode<
+  P extends IParaNode & IParaNodeMethod<P, T>,
+  T extends IParaNode
+>(parent: P, currentIndex: number, onlyInParent?: boolean): T | null {
+  if (onlyInParent) {
+    const nextNode = parent.findChildrenByIndex(currentIndex + 1);
     if (nextNode) return nextNode;
-    if (onlyInParent && nextNode === null) return null;
-
-    const nextParent = parent.next();
-    if (nextParent) {
-        const nextNode = nextParent.findChildrenByIndex(0);
-        return nextNode;
-    }
-
     return null;
+  }
+
+  const nextParent = parent.next();
+  if (nextParent) {
+    const nextNode = nextParent.findChildrenByIndex(0);
+    return nextNode;
+  }
+
+  return null;
 }
 
-export function prevNode<P extends IParaNode & IParaNodeMethod<P, T>, T extends IParaNode>(
-    parent: P, onlyInParent?: boolean
-): T | null {
-    const { index } = parent;
-    const prevNode = parent.findChildrenByIndex(index - 1);
+export function prevNode<
+  P extends IParaNode & IParaNodeMethod<P, T>,
+  T extends IParaNode
+>(parent: P, currentIndex: number, onlyInParent?: boolean): T | null {
+  if (onlyInParent) {
+    const prevNode = parent.findChildrenByIndex(currentIndex - 1);
     if (prevNode) return prevNode;
-    if (onlyInParent && prevNode === null) return null;
-
-    const prevParent = parent.prev();
-    if (prevParent) {
-        const lastIndexOfSentence =  1;
-        const prevNode = prevParent.findChildrenByIndex(lastIndexOfSentence);
-        return prevNode;
-    }
     return null;
-}
+  }
 
+  const prevParent = parent.prev();
+  if (prevParent) {
+    const lastIndexOfSentence = 1;
+    const prevNode = prevParent.findChildrenByIndex(lastIndexOfSentence);
+    return prevNode;
+  }
+
+  return null;
+}
 
 // export function findNextNode<T>(only): T {
 
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,48 +1,50 @@
-import Sentence from './Sentence';
-import Text from './Text';
-import Paragraph from './Paragraph';
-import Word from './Word';
-import Character from './Character';
+import Sentence from "./Sentence";
+import Paragraph from "./Paragraph";
+import Word from "./Word";
+import Character from "./Character";
+import Paratree from ".";
 
 export interface IParaNode {
-    id: string;
-    index: number;
-    text: string;
+  id: string;
+  index: number;
+  text: string;
+  data: Map<string, any>;
 }
 
 export interface IParaNodeMethod<T extends IParaNode, C extends IParaNode> {
-    findChildrenById(id: string): C | null;
-    findChildrenByIndex(index: number): C | null;
-    next(onlyInParent?: boolean): T | null;
-    prev(onlyInParent?: boolean): T | null;
+  findChildrenById(id: string): C | null;
+  findChildrenByIndex(index: number): C | null;
+  next(onlyInParent?: boolean): T | null;
+  prev(onlyInParent?: boolean): T | null;
 }
 
-export interface IText {
-    paragraphs: Paragraph[];
-    findParagraphById(id: string): Paragraph | null;
-    findParagraphByIndex(index: number): Paragraph | null;
-}
+// export interface IText {
+//     paragraphs: Paragraph[];
+//     findParagraphById(id: string): Paragraph | null;
+//     findParagraphByIndex(index: number): Paragraph | null;
+// }
 
 export interface IParagraph {
-    sentences: Sentence[];
-    parent: Text;
-    findSentenceById(id: string): Sentence | null;
-    findSentenceByIndex(index: number): Sentence | null;
+  sentences: Sentence[];
+  parent: Paratree;
+  findSentenceById(id: string): Sentence | null;
+  findSentenceByIndex(index: number): Sentence | null;
 }
 
 export interface ISentence {
-    parent: Paragraph;
-    words: Word[];
-    findWordById(id: string): Word | null;
-    findWordByIndex(index: number): Word | null;
+  parent: Paragraph;
+  words: Word[];
+  findWordById(id: string): Word | null;
+  findWordByIndex(index: number): Word | null;
 }
 
 export interface IWord {
-    parent: Sentence;
-    chars: Character[];
-    charCount(): number;
+  parent: Sentence;
+  chars: Character[];
+  clean: string;
+  charCount(): number;
 }
 
 export interface ICharacter {
-    parent: Word;
+  parent: Word;
 }
